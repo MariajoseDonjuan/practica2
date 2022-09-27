e61dac652a856ea745c4fed8af7582eeefe8e0bb
MariajoseDonjuan <marijosedonjuan@gmail.com>
2022-09-27 17:30:52 -0500

    practica2

---
+++ lib/include.h
@@ -0,0 +1,21 @@
+
+#ifndef INCLUDE_H_
+#define INCLUDE_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+volatile uint16_t ui32Loop;
+unsigned long Led;
+#include "TM4C1294NCPDT.h"
+#include "GPIO.h"
+
+
+
+
+
+
+#endif /* INCLUDE_H_ */
