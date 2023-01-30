43a7d355d400656e949cd8c6a48db4d33af0cbb0
MariajoseDonjuan <112989058+MariajoseDonjuan@users.noreply.github.com>
2023-01-29 21:39:16 -0600

    Aquí están los archivos y códigos de la practica 2 en equipos.

---
+++ Practica_2TM4C129/lib/include.h
@@ -0,0 +1,20 @@
+
+#ifndef INCLUDE_H_
+#define INCLUDE_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "system_TM4C129.h"
+#include "TM4C1294NCPDT.h"
+#include "GPIO.h"
+
+
+
+
+
+
+#endif /* INCLUDE_H_ */
---
+++ Practica_2TM4C129/main.c
@@ -0,0 +1,82 @@
+#include <stdint.h>
+#include "../inc/tm4c1294ncpdt.h"
+
+
+#include "lib/include.h"
+
+#define SENSOR  (*((volatile uint32_t *)0x4002400C))
+
+#define LIGHT   (*((volatile uint32_t *)0x400050FC))
+
+// Linked data structure
+
+struct State {
+
+uint16_t Out;
+
+uint32_t Time; 
+
+uint32_t Next[4];};
+
+typedef const struct State State_t;
+
+#define goN   0
+
+#define waitN 1
+
+#define goE   2
+
+#define waitE 3
+
+State_t FSM[4]={
+
+ {0x21,10000,{goN,waitN,goN,waitN}},
+
+ {0x22,5000,{goE,goE,goE,goE}},
+
+ {0x0C,10000,{goE,goE,waitE,waitE}},
+
+ {0x14,5000,{goN,goN,goN,goN}}};
+
+volatile uint32_t S;  // index to the current state
+
+volatile uint32_t Input;
+
+int main(void){ volatile uint32_t delay;
+
+ SYSCTL_RCGCGPIO_R = SYSCTL_RCGCGPIO_R1|SYSCTL_RCGCGPIO_R4;
+
+
+  SystemClock_120MHz();       // 80 MHz, Program 10.1
+
+  SysTick_Init();   // Program 10.2
+
+  GPIO_Config();
+
+  SYSCTL_RCGCGPIO_R  |= 0x12;      // B E
+
+  delay = SYSCTL_RCGCGPIO_R;      // no need to unlock
+
+  GPIO_PORTE_AHB_DIR_R &= ~0x03;   // inputs on PE1-0
+
+  GPIO_PORTE_AHB_DEN_R |= 0x03;    // enable digital on PE1-0
+
+  GPIO_PORTB_AHB_DIR_R |= 0x3F;    // outputs on PB5-0
+
+  GPIO_PORTB_AHB_DEN_R |= 0x3F;    // enable digital on PB5-0
+
+  S = goN;  
+
+  while(1){
+
+    LIGHT = FSM[S].Out;  // set lights
+
+    SysTick_Wait10ms(FSM[S].Time);
+
+    Input = SENSOR;     // read sensors
+
+    S = FSM[S].Next[Input]; 
+
+  }
+
+}
